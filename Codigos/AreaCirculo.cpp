aff9662f985b3a2e0bd66865a4d9a7bfc8559a1b
Marina Cadete <147002557+marinacadete@users.noreply.github.com>
2024-07-24 17:47:33 -0300

    Area Circulo

---
+++ Codigos/AreaCirculo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+ 
+using namespace std;
+ 
+int main() {
+    double A,n,r;
+    n = 3.14159;
+    scanf("%lf",&r);
+    A = n * pow(r,2);
+    printf("A=%.4lf\n",A);
+    return 0;
+}
