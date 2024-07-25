6a947d31d0501066103be9a38bf7f6f53eed2824
Marina Cadete <147002557+marinacadete@users.noreply.github.com>
2024-07-25 10:49:00 -0300

    Números Pares

---
+++ Codigos/NumerosPares.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    int N,n;
+    N = 2;
+    
+    while (N<=100){
+        printf("%d\n",N);
+        N = N + 2;
+    }
+    return 0;
+}
