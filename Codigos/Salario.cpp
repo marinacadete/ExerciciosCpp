bad3ad375fe6bb807da6f57eda93998a8094f8ee
Marina Cadete <147002557+marinacadete@users.noreply.github.com>
2024-07-25 10:10:18 -0300

    Salário

---
+++ Codigos/Salario.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    int n,h;
+    double ph,s;
+    scanf("%d%d%lf",&n,&h,&ph);
+    
+    s = h * ph;
+    
+    printf("NUMBER = %d\n",n);
+    printf("SALARY = U$ %.2lf\n",s);
+ 
+    return 0;
+}
