f302a634c98fbefb276df6e2e7d5dbbe9c42643e
Marina Cadete <147002557+marinacadete@users.noreply.github.com>
2024-07-25 10:24:24 -0300

    Conversao Tempo

---
+++ Codigos/ConversaoTempo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    int N,h,m,s;
+    scanf("%d",&N);
+    h = N / 3600;
+    m = (N % 3600) / 60;
+    s = (N % 3600) % 60;
+    printf("%d:%d:%d\n",h,m,s);
+    return 0;
+}
